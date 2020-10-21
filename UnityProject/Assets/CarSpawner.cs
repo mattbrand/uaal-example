f7638fd845dae1900d6229c74b6e2de3aaadfba2
Robert Boyle <rboyle@cainc.com>
2020-10-21 14:24:55 -0700

    Added car model and spawner

---
+++ UnityProject/Assets/CarSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    public GameObject carPrefab;
+
+    private int count = 0;
+    
+    private void Update()
+    {
+        if (count > 5000)
+            return;
+
+        GameObject newCar = GameObject.Instantiate(carPrefab);
+        newCar.transform.position = new Vector3(4.0f + count, -1.9f, 0.0f);
+        newCar.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+
+        count++;
+    }
+
+    public void Spawn100Cars()
+    {
+        
+    }
+}
