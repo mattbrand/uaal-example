51f598361a2b0d96ab3327926a029f554f55ddc9
Robert Boyle <rboyle@cainc.com>
2020-10-21 14:26:22 -0700

    Added increase memory button to unity scene to spawn 500 cars

--- UnityProject/Assets/CarSpawner.cs
+++ UnityProject/Assets/CarSpawner.cs
@@ -7,21 +7,37 @@ public class CarSpawner : MonoBehaviour
     public GameObject carPrefab;
 
     private int count = 0;
+    private Transform parentTrans;
+    
+    public void Start()
+    {
+        parentTrans = GameObject.Find("CarSpawner").transform;
+    }
     
     private void Update()
     {
-        if (count > 5000)
-            return;
 
-        GameObject newCar = GameObject.Instantiate(carPrefab);
-        newCar.transform.position = new Vector3(4.0f + count, -1.9f, 0.0f);
-        newCar.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
-
-        count++;
     }
-
-    public void Spawn100Cars()
+    
+    public void OnButtonClicked()
     {
+        //Output this to console when the Button3 is clicked
+        Debug.Log("Adding 500 more models to increase memory");
+        Spawn500Cars();
+    }
+    
+    private void Spawn500Cars()
+    {
+        var pt = this.parentTrans;
         
+        for (int i = 0; i < 500; i++)
+        {
+            GameObject newCar = GameObject.Instantiate(carPrefab);
+            newCar.transform.position = new Vector3(4.0f + count + i, -1.9f, 0.0f);
+            newCar.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+            newCar.transform.parent = pt; // here to organize the clones under a single game object 
+        }
+
+        count = count + 500;
     }
 }
